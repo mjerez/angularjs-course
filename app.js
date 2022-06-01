80339fd622ec5a343cf9aa9cc26e51e36376c082
mjerez <felix_jerez@redmane.com>
2022-06-01 09:54:48 -0700

    adding some basic Angular stuff

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 (function(){
     'use strict';
 
+    console.log("Starting the angular app");
+
+    var x=1;
+
+    angular.module('MyAngularApp', [])
+    .controller("MyFirstController", function() {
+        console.log ("Starting the controller");
+    } );
 
 } ) ();
