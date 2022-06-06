e6334b2d07b83fb24eb089ad9e4fe226e91d1c4d
mjerez <felix_jerez@redmane.com>
2022-06-06 14:09:16 -0700

    some more functionality

--- app.js
+++ app.js
@@ -6,8 +6,45 @@
     var x=1;
 
     angular.module('MyAngularApp', [])
-    .controller("MyFirstController", function() {
+    .controller("MyFirstController", ['$scope', '$filter', '$injector' ,'$log', DIController]);
+    
+    function DIController (
+                                              $scope, 
+                                              $filter,
+                                              $injector,
+                                              $log) {
         console.log ("Starting the controller");
-    } );
+
+        $scope.name=""; 
+        $scope.totalValue=0;
+
+        $scope.displayNumeric=function () {
+            var totalNameValue=calculateNumerForString($scope.name);
+            $scope.totalValue=totalNameValue;
+        }
+
+        function calculateNumerForString(string) {
+            var totalStringValue=0;
+            for (var i=0;i<string.length;i++) {
+                totalStringValue+=string.charCodeAt(i);
+            }
+            return totalStringValue;
+        }
+
+
+        $scope.sayHello = function () {
+            return "Hi there!";
+        }
+
+        $scope.upperCase=function () {
+            var upCase=$filter("uppercase");
+            $scope.textInput=upCase($scope.textInput);
+        }
+
+        console.log($injector.annotate(DIController));
+
+        $log.log("Hola");
+
+    } ;
 
 } ) ();
